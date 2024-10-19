7e57ccacae874734a699c4e1b0dc5e3719069e00
M. Danyal Kayani <mdanyalkayani.mdk@gmail.com>
2024-10-19 21:36:35 +0500

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("menuButton").addEventListener("click", function () {
-  const sidebar = document.getElementById("sidebar");
-  if (sidebar.style.left === "0px") {
-    sidebar.style.left = "-260px";
-  } else {
-    sidebar.style.left = "0px";
-  }
-});
